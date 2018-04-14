fdc76ee7bb984fcaf2301accdf12463f61dfa21b
James Little <littleguy23@gmail.com>
2018-04-14 15:21:50 +0200

    Build HTTP server on top of WebSockets server, fix async DB delete

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const server = require('http').createServer(require('express')());
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 
-// Delete old DB file
-fs.stat('./db/main.db', function (err, stats) {
-	console.log(stats);
-
-	if(err) {
-		return console.error(err);
-	}
-
-	fs.unlink('./db/main.db', function(err) {
-		if(err) return console.log(err);
-		console.log('Old database deleted successfully');
-	});
-});
+try {
+	fs.unlinkSync('./db/main.db');
+	console.log('successfully deleted old database file');
+} catch (err) {
+	console.log('Could not delete old database file... did it exist?')
+}
 
 var db = new sqlite3.Database('./db/main.db');
 
@@ -52,24 +47,23 @@ db.serialize(function() {
 			private_key_enc blob NOT NULL,
 			created_at text NOT NULL,
 			expired_at text)`);
-
-	db.run(``)
 });
 
 db.close();
 
+console.log("Database schema written");
+
 // API Endpoints
-app.get('/', function (req, res) {
-	res.send('hello world')
-});
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 server.listen(8080);
 
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log('A user connected');
 
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		console.log('A user disconnected');
 	});
 
 	socket.on('msg', function(msg){
