87a04191d5ee5e4140cb24d7bf721746cefd2703
James Little <littleguy23@gmail.com>
2018-05-08 13:32:42 +0200

    Add login functionality, move keygen to client, change data access model

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express    = require('express')
 const app        = express()
 const server     = require('http').createServer(app);
 const io         = require('socket.io').listen(server);
-const sqlite3    = require('sqlite3').verbose();
+const sqlite3    = require('sqlite3');
 const fs         = require('fs');
 const NodeRSA    = require('node-rsa');
 const bodyParser = require('body-parser');
@@ -71,6 +71,10 @@ db.serialize(function() {
 	db.run(`INSERT INTO chats default values`);
 	db.run(`INSERT INTO chats default values`);
 
+	db.run(`INSERT INTO users (username, password, first_name, last_name) VALUES ("jameslittle", "$2b$10$poJgedWL57PEMaDHOt/MkuXJmJH4Cw1lfa5MjJitJGcaStEmRqyI2", "James", "Little")`);
+	db.run(`INSERT INTO users (username, password, first_name, last_name) VALUES ("maddie", "$2b$10$poJgedWL57PEMaDHOt/MkuXJmJH4Cw1lfa5MjJitJGcaStEmRqyI2", "Maddie", "Tucker")`);
+	db.run(`INSERT INTO users (username, password, first_name, last_name) VALUES ("danny", "$2b$10$poJgedWL57PEMaDHOt/MkuXJmJH4Cw1lfa5MjJitJGcaStEmRqyI2", "Danny", "Little")`);
+
 	db.run(`INSERT INTO user_chat (user_id, chat_id) VALUES (1, 1)`);
 	db.run(`INSERT INTO user_chat (user_id, chat_id) VALUES (2, 1)`);
 	db.run(`INSERT INTO user_chat (user_id, chat_id) VALUES (2, 2)`);
@@ -90,107 +94,86 @@ db.serialize(function() {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, socket_id");
   next();
 });
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/messages', function (req, res) {
-	if(!req.query.recipient || !/^\d+$/.test(req.query.recipient)) {
-		return res.status(404).send("recipient error");
-	}
+/** kill me now **/
+app.get('/loadUserData', function(req, res) {
+	var keys, chats, messages;
+	var u_id = 1;
 
-	var r_id = req.query.recipient;
-	db.all(`SELECT * FROM messages WHERE recipient_id = ` + r_id, function(err, data) {
-		if(err) return res.status(404).send(err);
-		return res.send(data);
-	});
-});
+	db.all(`SELECT * FROM keys WHERE user_id = ` + u_id + ` OR expired_at IS NULL`, function(err, data) {
+		if(err || !data) return res.status(404).send(err || "No data found for ID");
+		keys = data;
 
-app.get('/users', function(req, res) {
-	db.all(`SELECT * FROM users`, function(err, data) {
-		if(err) return res.status(404).send(err);
-		return res.send(data);
-	});
-});
+		db.all(`SELECT * FROM messages WHERE recipient_id = ` + u_id, function(err, data) {
+			if(err || !data) return res.status(404).send(err || "No data found for ID");
+			messages = data;
 
-app.get('/chats', function(req, res) {
-	if(!req.query.user_id || !/^\d+$/.test(req.query.user_id)) {
-		return res.status(404).send("user id error");
-	}
+			db.all(`SELECT * FROM chats WHERE chat_id IN (SELECT chat_id FROM user_chat WHERE user_id IS ` + u_id + `)`, function(err, data) {
+				if(err || !data) return res.status(404).send(err || "No data found for ID");
+				chats = data;
+				var chatsProcessed = 0;
 
-	var chats = [];
+				db.serialize(function() {
+					for (let chat of chats) {
+						db.all(`SELECT user_id, first_name, last_name FROM users WHERE user_id IN 
+						(SELECT user_id FROM user_chat WHERE chat_id IS ` + chat.chat_id + `)`, 
+						function(err, data) {
+							if(err) return res.status(404).send(err);
 
-	function getUsersInChats() {
-		var chatsProcessed = 0;
-		for (let chat of chats) {
-			db.all(`SELECT user_id, first_name, last_name FROM users WHERE user_id IN (SELECT user_id FROM user_chat WHERE chat_id IS ` + chat.chat_id + `)`, function(err, data) {
-				if(err) return res.status(404).send(err);
-
-				chat.members = data;
-				chatsProcessed++;
-				if(chatsProcessed == chats.length) {
-					res.send(chats);
-					return;
-				}
+							chat.members = data;
+							chatsProcessed++;
+							if(chatsProcessed == chats.length) {
+								return res.send({
+									"keys": keys,
+									"messages": messages,
+									"chats": chats
+								});
+							}
+						});
+					};
+				});
 			});
-		};
-	};
-
-	var u_id = req.query.user_id;
-	db.serialize(function() {
-		db.all(`SELECT * FROM chats WHERE chat_id IN (SELECT chat_id FROM user_chat WHERE user_id IS ` + u_id + `)`, function(err, data) {
-			if(err) return res.status(404).send(err);
-
-			chats = data;
-			db.serialize(getUsersInChats);
 		});
 	});
 });
 
-app.get('/publicKey', function(req, res) {
-	if(!req.query.user_id || !/^\d+$/.test(req.query.user_id)) {
-		return res.status(404).send("user id error");
-	}
-
-	var u_id = req.query.user_id;
-	db.get(`SELECT public_key FROM keys WHERE user_id = ` + u_id + ` LIMIT 1`, function(err, data) {
-		if(err) return res.status(404).send(err);
-		return res.send(data.public_key);
-	});
-});
-
-app.get('/privateKey', function(req, res) {
-	if(!req.query.user_id || !/^\d+$/.test(req.query.user_id)) {
-		return res.status(404).send("user id error");
-	}
-
-	var u_id = req.query.user_id;
-	db.get(`SELECT private_key_enc FROM keys WHERE user_id = ` + u_id + ` LIMIT 1`, function(err, data) {
-		if(err) return res.status(404).send(err);
-		return res.send(data.private_key_enc);
-	});
-});
-
 app.post('/login', function(req, res) {
+	// Check username validity
 	if(!req.body.username || !/^[0-9A-Za-z_\-\.]+$/.test(req.body.username)) {
 		return res.status(404).send("username error");
 	}
 
 	var username = req.body.username;
 	var plaintextPassword = req.body.password;
-	db.get(`SELECT password, user_id FROM users WHERE username = "` + username + `" LIMIT 1`, function(err, data) {
+	db.get(`SELECT * FROM users WHERE username = "` + username + `" LIMIT 1`, function(err, data) {
 		if(err) return res.status(404).send("db error");
 		if(!data || !data.password) return res.send("no such entry");
 
 		var hash = data.password;
 		var u_id = data.user_id;
+
 		bcrypt.compare(plaintextPassword, hash, function(err, valid) {
-			if(err) return res.status(500).send("hash error");
-			updateUserKeypair(u_id, function(err, data) {
-				return res.send(valid);
+			if(err) return res.status(404).send("password did not match");
+
+			// When we're here we have successfully logged in
+			var socket_id = req.headers.socket_id;
+			if(connections[u_id]) {
+				connections[u_id].push(socket_id)
+			} else {
+				connections[u_id] = [socket_id];
+			}
+
+			console.log(connections);
+
+			return res.send({
+				success: true,
+				user: data
 			});
 		});
 	});
@@ -216,7 +199,7 @@ app.post('/createUser', function(req, res) {
 
 			db.run(`INSERT INTO users (username, password, first_name, last_name) VALUES
 			("` + username + `", "` + hash + `", "` + first + `", "` + last + `")`, function(err) {
-				if(err) return res.send(err);
+				if(err) return res.status(500).send(err);
 				updateUserKeypair(this.lastID, function(err, result) {
 					return res.send(true);
 				});
@@ -224,6 +207,8 @@ app.post('/createUser', function(req, res) {
 		});
 	});
 });
+
+app.post('/')
 
 server.listen(8080);
 
@@ -255,34 +240,20 @@ function parseMessage(msg) {
 	};
 }
 
-function getSocketID(user_id) {
-	Object.prototype.getKeyByValue = function( value ) {
-		for( var prop in this ) {
-			if( this.hasOwnProperty( prop ) ) {
-				if( this[ prop ] === value ) return prop;
-			}
-		}
-	}
-
-	var socketID = connections.getKeyByValue(user_id);
-	return socketID;
-}
-
 io.on('connection', function(socket){
 	console.log('Connection ' + socket.id + ' began');
-	connections[socket.id] = 0;
 
 	socket.on('disconnect', function(){
-		delete connections[socket.id];
-		console.log('Connection ' + socket.id + ' ended');
-		console.log(connections);
-	});
-
-	socket.on('login', function(msg) {
-		if(connections[socket.id] === 0) {
-			connections[socket.id] = msg;
+		console.log('Connection ' + socket.id + ' ended :(');
+		for(user_id in connections) {
+			if (connections[user_id].indexOf(socket.id) != -1) {
+				connections[user_id].splice(connections[user_id].indexOf(socket.id), 1);
+				if(connections[user_id].length == 0) {
+					delete connections[user_id];
+					break;
+				}
+			}
 		}
-
 		console.log(connections);
 	});
 
@@ -300,11 +271,29 @@ io.on('connection', function(socket){
 								  values ("` + msg.content + `", ` + msg.snd + `, ` + msg.rcv + `, ` + msg.cht + `)`);
 		});
 
-		var socketID = getSocketID(msg.rcv);
 
-		if(socketID != undefined) {
-			console.log("Sending message to socketid", getSocketID(msg.rcv));
-			io.to(getSocketID(msg.rcv)).emit('msg', msg.content);
+		if(connections[msg.rcv]) {
+			for (socket_id in connections[msg.rcv]) {
+				console.log("Sending message to socketid", socket_id);
+				io.to(socket_id).emit('msg', msg.content);
+			}
 		}
 	});
+
+	socket.on('key-submit', function(data) {
+		console.log(data);
+		let s_id = data.socket_id;
+		let u_id = data.user_id;
+		let public = data.key.public;
+		let private = data.key.private;
+
+		db.serialize(function() {
+			db.run(`UPDATE keys SET expired_at = datetime('now') WHERE expired_at IS NULL AND user_id = ` + u_id);
+			db.run(`INSERT INTO keys (user_id, public_key, private_key_enc) 
+				VALUES (` + u_id + `, "` + public + `", "` + private + `")`, function() {
+					console.log("Done with database stuff")
+					io.to(s_id).emit('key-response', {success: true});
+				});
+		});
+	})
 });
