e8ddb33af10660ec864ef69202fcbc1cb13a5d03
James Little <littleguy23@gmail.com>
2018-05-12 15:52:00 +0200

    Add register endpoint to rewrite

--- index2.js
+++ index2.js
@@ -51,6 +51,24 @@ function removeFromConnections(socket_id) {
 	}
 }
 
+/** Get Data **/
+
+async function getAllUsers() {
+	const users = await db.all(`SELECT user_id, username, first_name, last_name FROM users`);
+	return users;
+}
+
+/** Update Keypair **/
+
+async function updateUserKeypair(user_id) {
+	var key = new NodeRSA();
+	key.generateKeyPair(1024);
+
+	await db.run(`UPDATE keys SET expired_at = datetime('now') WHERE expired_at IS NULL AND user_id = ?`, [user_id]);
+	await db.run(`INSERT INTO keys (user_id, public_key, private_key_enc) VALUES (?, ?, ?)`, 
+		[user_id, key.exportKey('public'), key.exportKey('private')]);
+};
+
 /** Request Parameter Testing **/
 
 function requestParameterDoesNotExistException() {
@@ -107,7 +125,6 @@ app.post('/login', async function(req, res) {
 
 	try {
 		const data = await db.get(`SELECT * FROM users WHERE username = ? LIMIT 1`, [username]);
-		console.log(data);
 
 		const match = await bcrypt.compare(plaintextPassword, data.password);
 		if(!match) throw new loginPasswordMismatchException()
@@ -124,10 +141,62 @@ app.post('/login', async function(req, res) {
 	}
 });
 
+/** Register Endpoint **/
+
+function registerUserAlreadyExistsException(username) {
+	return {
+		error: true,
+		message: "Username " + username + " already exists in database."
+	}
+}
+
+app.post('/register', async function(req, res) {
+	try { 
+		testRequestParameter(req.body.username, /^[0-9A-Za-z_\-\.]+$/);
+		testRequestParameter(req.body.password, /.{8,}/);
+		testRequestParameter(req.body.first, /^[0-9A-Za-z]+$/);
+		testRequestParameter(req.body.last, /^[0-9A-Za-z]+$/);
+	} 
+	catch(e) {return res.status(400).send(e) }
+
+	var username = req.body.username;
+	var plaintextPassword = req.body.password;
+	var first = req.body.first;
+	var last = req.body.last;
+
+	try {
+		const userSearchData = await db.get(`SELECT password FROM users WHERE username = ? LIMIT 1`, [username]);
+		if(userSearchData) throw new registerUserAlreadyExistsException(username);
+
+		const passwordHash = await bcrypt.hash(plaintextPassword, bcryptSaltRounds);
+
+		const newUser = await db.run(`INSERT INTO users (username, password, first_name, last_name) VALUES (?, ?, ?, ?)`, [username, passwordHash, first, last]);
+		
+		await updateUserKeypair(newUser.lastID);
+		
+		const singleUserData = await db.get(`SELECT * FROM users WHERE user_id = ? LIMIT 1`, [newUser.lastID]);
+		const allUsersData = await getAllUsers();
+
+		io.of('/').emit('users-reload');
+		return res.send({
+			success: true,
+			user: singleUserData
+		});
+	} catch(error) {
+		return res.status(400).send(error);
+	}
+});
+
+
 /** Run **/
 var dbPromise = sqlite.open('./db/main.db', { Promise });
 dbPromise.then((dbFromPromise) => {
 	db = dbFromPromise;
 	server.listen(8080);
 	console.log("Server started");
+})
+
+io.on('connection', function(socket) {
+	socket.on('disconnect', () => {handleDisconnect(socket.id)});
+	socket.on('msg', (m) => {handleIncomingMessage(m)});
 })
